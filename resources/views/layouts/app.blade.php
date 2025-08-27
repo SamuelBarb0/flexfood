cec32b047c0924c6faab7dbc19802384cddf755e
Samuel <sambarbosa66@gmail.com>
2025-08-27 17:13:36 -0500

    FlexFood 3.6.3.0.9nd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,11 +21,14 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    
-    @if(!empty($settings?->favicon_path))
-    <link rel="icon" href="{{ asset($settings->favicon_path) }}">
-    <link rel="shortcut icon" href="{{ asset($settings->favicon_path) }}">
-    @endif
+
+    {{-- Favicon con fallback al logo (y luego a FlexFood) --}}
+    @php
+        $faviconPath = $settings?->favicon_path ?: $settings?->logo_path ?: 'images/flexfood.png';
+    @endphp
+    <link rel="icon" href="{{ asset($faviconPath) }}">
+    <link rel="shortcut icon" href="{{ asset($faviconPath) }}">
+    <link rel="apple-touch-icon" href="{{ asset($faviconPath) }}">
 
     {{-- CSS crítico para prevenir problemas de zoom --}}
     <style>
@@ -134,8 +137,6 @@
     <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-
 
     @stack('scripts')
 </body>
